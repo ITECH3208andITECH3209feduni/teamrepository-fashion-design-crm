29f162b0eeccb0966c70ff4052ab035969c6a958
Sukhbir05 <70251949+Sukhbir05@users.noreply.github.com>
2021-05-05 20:28:38 +1000

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,55 @@
+  <main>
+
+       
+<div class="hero-area section-bg2">
+<div class="container">
+<div class="row">
+<div class="col-xl-12">
+<div class="slider-area">
+<div class="slider-height2 slider-bg4 d-flex align-items-center justify-content-center">
+<div class="hero-caption hero-caption2">
+<h2>Cart</h2>
+<nav aria-label="breadcrumb">
+<ol class="breadcrumb justify-content-center">
+<li class="breadcrumb-item"><a href="<?=APP_URL?>">Home</a></li>
+<li class="breadcrumb-item"><a href="#">Cart</a></li>
+</ol>
+</nav>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<section class="cart_area">
+<div class="container">
+<div class="cart_inner">
+<div class="table-responsive">
+<table class="table ">
+<thead>
+<tr>
+<th scope="col">Product</th>
+<th scope="col">Price</th>
+<th scope="col">Discount</th>
+<th scope="col">Quantity</th>
+<th scope="col">Total</th>
+<th scope="col">Action</th>
+</tr>
+</thead>
+<tbody class="cartpagelist">
+
+</tbody>
+</table>
+<div class="checkout_btn_inner float-right">
+<a class="btn" href="#">Continue Shopping</a>
+<a class="btn checkout_btn" href="#">Proceed to checkout</a>
+</div>
+</div>
+</div>
+</div>
+
+
+</section>
